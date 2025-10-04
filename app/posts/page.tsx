39a4806efa487db3fa4379c4def66a7c4378031d
Bosnier <notreplybosnier@outlook.com>
2025-10-04 13:16:15 +0400

    Implement posts page

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,3 +1,21 @@
-export default function Page() {
-  return <div>list of posts</div>
+import { prisma } from "@/prisma"
+import Post from "@/app/components/post"
+
+export default async function Page() {
+  const posts = await prisma.post.findMany({
+    where: { published: true },
+    take: 10,
+    include: { author: true },
+    orderBy: { createdAt: "desc" },
+  })
+
+  return (
+    <main className="px-4 flex flex-col gap-5 max-w-3xl mx-auto w-full pb-8">
+      <h1 className="text-2xl font-bold font-serif">All posts</h1>
+
+      {posts.map((post) => (
+        <Post post={post} key={post.id} />
+      ))}
+    </main>
+  )
 }
