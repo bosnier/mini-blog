6a19ad8adeb3205918959a9039f7e7078e01216f
Bosnier <notreplybosnier@outlook.com>
2025-10-04 13:04:18 +0400

    Remove unneccessary import and change padding on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Post, { PostType } from "@/app/components/post"
+import Post from "@/app/components/post"
 import { Button } from "@/components/ui/button"
 import { prisma } from "@/prisma"
 import { Plus } from "lucide-react"
@@ -10,7 +10,7 @@ export default async function Home() {
   const posts = await prisma.post.findMany({ where: { published: true }, take: 4, include: { author: true } })
 
   return (
-    <main className="p-2 flex flex-col gap-5 max-w-3xl mx-auto w-full">
+    <main className="px-4 flex flex-col gap-5 max-w-3xl mx-auto w-full">
       <h1 className="text-2xl font-bold font-serif">Latest posts</h1>
       {posts.map((post) => (
         <Post post={post} key={post.id} />
@@ -18,8 +18,8 @@ export default async function Home() {
       <div className="flex gap-1 mt-2">
         <Link href="/posts" className="grow">
           <Button variant="default" className="w-full">
-          Read more
-        </Button>
+            Read more
+          </Button>
         </Link>
         <Button variant="outline" className="self-start">
           <Plus />
