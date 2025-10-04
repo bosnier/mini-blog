193ab297958f9e09b256a17c1575fb5506f35980
Bosnier <notreplybosnier@outlook.com>
2025-10-04 13:18:27 +0400

    Implement certain post page

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,5 +1,21 @@
+import { prisma } from "@/prisma"
+import Link from "next/link"
+
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params
+  const post = await prisma.post.findFirst({ where: { id }, include: { author: true } })
 
-  return <div>post page id: {id}</div>
+  // TODO should handle not found error
+  if (!post) return "not found"
+
+  return (
+    <main className="px-4 flex flex-col gap-2 max-w-3xl mx-auto w-full pb-8">
+      <h2 className="text-2xl font-bold font-serif">{post.title}</h2>
+      <div className="flex justify-between">
+        <Link href={"/profile/" + post.author.id}>{post.author.name}</Link>
+        <span className="self-end ">{post.createdAt.toLocaleString()}</span>
+      </div>
+      <span className="">{post.content}</span>
+    </main>
+  )
 }
