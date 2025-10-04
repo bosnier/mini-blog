9a0526e0f51784beb3c56ee67ca4eca15190036a
Bosnier <notreplybosnier@outlook.com>
2025-10-04 20:45:33 +0400

    Show actually latest posts on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,14 @@ import { Plus } from "lucide-react"
 import Link from "next/link"
 
 export default async function Home() {
-  console.log(await prisma.user.findMany())
-
-  const posts = await prisma.post.findMany({ where: { published: true }, take: 4, include: { author: true } })
+  // TODO isr
+  
+  const posts = await prisma.post.findMany({
+    where: { published: true },
+    take: 4,
+    include: { author: true },
+    orderBy: { createdAt: "desc" },
+  })
 
   return (
     <main className="px-4 flex flex-col gap-5 max-w-3xl mx-auto w-full">
