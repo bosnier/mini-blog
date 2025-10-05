588ea94bd502ba0317ca73e4910894137c666ec8
Bosnier <notreplybosnier@outlook.com>
2025-10-05 15:48:13 +0400

    Revert "Implement author search in posts filters"
    This reverts commit 759df3f300fcae8f639276977c6ab6496f6025a7.

--- app/components/posts-filters.tsx
+++ app/components/posts-filters.tsx
@@ -1,10 +1,13 @@
 "use client"
 
+import { Button } from "@/components/ui/button"
+import { Command, CommandEmpty, CommandInput, CommandList } from "@/components/ui/command"
 import { Input } from "@/components/ui/input"
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
+import { UserRoundSearch } from "lucide-react"
 import { usePathname, useRouter, useSearchParams } from "next/navigation"
 import { parsePostsFilters, PostsSearchParams } from "@/app/lib/posts-filters-utils"
-import { AsyncSelect } from "@/components/ui/async-select"
 import { useEffect, useState } from "react"
 import { useDebounce } from "../lib/hooks/use-debounce"
 
@@ -18,14 +21,6 @@ export default function PostsFilters({ filterAuthor }: { filterAuthor?: boolean 
   const [query, setQuery] = useState(q || "")
   const debouncedQuery = useDebounce(query, 300)
   useEffect(() => updateURL({ q: debouncedQuery }), [debouncedQuery])
-
-  const [selectedAuthor, setSelectedAuthor] = useState(author || "")
-
-  function setAuthor(authorId: string) {
-    setSelectedAuthor(authorId)
-    updateURL({ author: authorId })
-  }
-
   function updateURL(newParams: PostsSearchParams) {
     const params = new URLSearchParams(searchParams)
     Object.entries(newParams).forEach(([name, value]) => params.set(name, value))
@@ -42,24 +37,22 @@ export default function PostsFilters({ filterAuthor }: { filterAuthor?: boolean 
         className="w-auto grow min-w-64"
       />
       {filterAuthor && (
-        <AsyncSelect
-          fetcher={
-            async (query?: string) =>
-              fetch("/api/users/search?q=" + query)
-                .then((r) => r.json())
-                .then((r) => r.users as { id: string; name: string }[])
-            // TODO catch errors?
-          }
-          renderOption={(user) => user.name}
-          getOptionValue={(user) => user.id}
-          getDisplayValue={(user) => "By " + user.name}
-          notFound={<div className="px-3 pt-2 text-sm">No authors found</div>}
-          label="Author"
-          placeholder="Find author..."
-          value={selectedAuthor}
-          onChange={setAuthor}
-          width="25ch"
-        />
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button variant="outline">
+              By author
+              <UserRoundSearch />
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent>
+            <Command>
+              <CommandInput placeholder="Find author..." />
+              <CommandList>
+                <CommandEmpty>No author found</CommandEmpty>
+              </CommandList>
+            </Command>
+          </PopoverContent>
+        </Popover>
       )}
       <Select defaultValue={sort || "newer"} onValueChange={(value: "newer" | "older") => updateURL({ sort: value })}>
         <SelectTrigger>
