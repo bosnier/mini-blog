ccd5ed237d58a4832a815e23d0a93220f64db525
Bosnier <notreplybosnier@outlook.com>
2025-10-07 22:18:08 +0400

    Add `truncateContent` and `includeUnpublished` query params to `/api/posts/get` endpoint

--- app/api/posts/get/route.ts
+++ app/api/posts/get/route.ts
@@ -1,25 +1,27 @@
 import { parsePostsFilters } from "@/app/lib/posts-filters-utils"
+import { auth } from "@/auth"
 import { prisma } from "@/prisma"
 import { NextRequest, NextResponse } from "next/server"
-
-// TODO add `trimContent` and `includeUnpublished` flags
-// don't forget to check ownership
 
 export async function GET(req: NextRequest) {
   const url = new URL(req.url)
   const searchParams = url.searchParams
+  const session = await auth()
 
   const { q, author, sort } = parsePostsFilters(url.searchParams)
-  const trimContent = searchParams.get("trimContent")
+  const truncateContent = searchParams.get("truncateContent")
+  const includeUnpublished = searchParams.get("includeUnpublished")
   const from = searchParams.get("from")
   const limit = Math.min(Math.max(parseInt(searchParams.get("limit") ?? "5"), 5), 100)
+
+  const showUnpublished = session?.user?.id === author && includeUnpublished === "true"
 
   const posts = await prisma.post.findMany({
     take: limit,
     skip: from ? 1 : 0,
     cursor: from ? { id: from } : undefined,
     where: {
-      published: true,
+      published: showUnpublished ? undefined : true,
       title: { contains: q, mode: "insensitive" },
       author: { id: author },
     },
@@ -28,6 +30,11 @@ export async function GET(req: NextRequest) {
   })
 
   const hasMore = posts.length === limit
+
+  if (truncateContent === "true") {
+    const truncatedPosts = posts.map((post) => ({ ...post, content: post.content.slice(0, 500) }))
+    return NextResponse.json({ posts: truncatedPosts, hasMore })
+  }
 
   return NextResponse.json({ posts, hasMore })
 }
