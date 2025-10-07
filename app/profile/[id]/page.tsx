955cbb08f41c416c4726ab23de0ee2b400d1d5bc
Bosnier <notreplybosnier@outlook.com>
2025-10-07 23:02:20 +0400

    Add `New post` button to own profile page

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -2,13 +2,17 @@ import PostsFilters from "@/app/components/posts-filters"
 import { PostsSearchParams } from "@/app/lib/posts-filters-utils"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { prisma } from "@/prisma"
-import { ImageOff } from "lucide-react"
+import { ImageOff, Plus } from "lucide-react"
 import PostsList from "@/app/components/posts-list"
+import { auth } from "@/auth"
+import { Button } from "@/components/ui/button"
 
 export default async function Page(props: {
   searchParams?: Promise<PostsSearchParams>
   params: Promise<{ id: string }>
 }) {
+  const session = await auth()
+
   const { id } = await props.params
 
   const user = await prisma.user.findFirst({ where: { id } })
@@ -23,14 +27,20 @@ export default async function Page(props: {
 
   return (
     <div className="px-4 flex flex-col gap-5">
-      <div className="flex gap-3 items-center">
+      <div className="flex gap-3 items-center flex-wrap">
         <Avatar className="size-16 -z-10">
           {user.image && <AvatarImage src={user.image} />}
           <AvatarFallback>
             <ImageOff />
           </AvatarFallback>
         </Avatar>
-        <span className="font-bold text-2xl">{user.name}</span>
+        <span className="font-bold text-2xl wrap-break-word overflow-hidden">{user.name}</span>
+        {session?.user?.id === id && (
+          <Button className="ms-auto">
+            <Plus />
+            New post
+          </Button>
+        )}
       </div>
 
       <PostsFilters />
