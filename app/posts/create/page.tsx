2090055117f6eda1be3db50e64f35fabb16c5693
Bosnier <notreplybosnier@outlook.com>
2025-10-08 19:06:01 +0400

    Protect post creation page

--- app/posts/create/page.tsx
+++ app/posts/create/page.tsx
@@ -1,6 +1,10 @@
 import PostCreationForm from "@/app/components/post-creation-form"
+import { auth } from "@/auth"
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth()
+  if (!session) return <div className="mx-auto text-2xl">You should sign in first</div>
+
   return (
     <div className="px-4 flex flex-col gap-2">
       <h2 className="text-2xl font-bold font-serif">Create post</h2>
